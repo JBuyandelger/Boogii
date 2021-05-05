db59cfb7ebe0c16b9b5b68756bf368153913257d
BuyanGIT <buyandelgerjan@gmail.com>
2021-05-05 10:57:27 +0800

    everything is fine

--- admin/manage-featured models.php
+++ admin/manage-featured models.php
@@ -81,7 +81,7 @@
               <tr>
                     <td><?php echo $sn++; ?>.</td>
                    <td><?php echo $title; ?></td> 
-                   <td>$<?php echo $price; ?></td> 
+                   <td>₮<?php echo $price; ?></td> 
                    
                    <td>
                        <?php 
